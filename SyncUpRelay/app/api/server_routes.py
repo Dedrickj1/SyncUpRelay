6893507dd306da3a7313ccfaf6e5eab6dfe0dc77
Dedrickj1 <dedrickjackson11@gmail.com>
2025-08-10 15:26:41 -0500

    Server feature done. I am able to GET, POST, PUT, and DELETE

--- SyncUpRelay/app/api/server_routes.py
+++ SyncUpRelay/app/api/server_routes.py
@@ -1,18 +1,75 @@
-from flask import Blueprint, jsonify
-from app.models import Server
+
+from flask import Blueprint, jsonify, request
+from flask_login import login_required, current_user
+from app.models import Server, db
+from app.forms import ServerForm
 
 # Create a Blueprint for server routes
 server_routes = Blueprint('servers', __name__)
 
-
+# Gets all Servers
 @server_routes.route('/')
 def get_all_servers():
-    
-    # Use the Server model to query all servers from the database
     servers = Server.query.all()
-    
-    # Convert the list of server objects into a list of dictionaries
     server_list = [server.to_dict() for server in servers]
-    
-    # Return the list of servers as a JSON response
     return jsonify(server_list)
+
+# Create Server
+@server_routes.route('/', methods=['POST'])
+@login_required
+def create_server():
+    form = ServerForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    
+    if form.validate_on_submit():
+        new_server = Server(
+            name=form.data['name'],
+            owner_id=current_user.id
+        )
+        db.session.add(new_server)
+        db.session.commit()
+        return new_server.to_dict()
+    
+    return {'errors': form.errors}, 400
+
+# Update Server
+@server_routes.route('/<int:server_id>', methods=['PUT'])
+@login_required
+def update_server(server_id):
+    
+    server = Server.query.get(server_id)
+
+    if not server:
+        return {'errors': 'Server not found'}, 404
+
+    if server.owner_id != current_user.id:
+        return {'errors': 'Forbidden'}, 403
+
+    form = ServerForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+
+    if form.validate_on_submit():
+        server.name = form.data['name']
+        db.session.commit()
+        return server.to_dict()
+        
+    return {'errors': form.errors}, 400
+
+
+# Delete Server
+@server_routes.route('/<int:server_id>', methods=['DELETE'])
+@login_required
+def delete_server(server_id):
+   
+    server = Server.query.get(server_id)
+
+    if not server:
+        return {'errors': 'Server not found'}, 404
+
+    if server.owner_id != current_user.id:
+        return {'errors': 'Forbidden'}, 403
+
+    db.session.delete(server)
+    db.session.commit()
+
+    return {'message': 'Server deleted successfully'}
