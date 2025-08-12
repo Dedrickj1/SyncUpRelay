3f999e355e030a7862c0aab4f3b7b862fabc6909
Dedrickj1 <dedrickjackson11@gmail.com>
2025-08-12 09:36:35 -0500

    finished the models and seeders for servers, channels, messages, and reactions

--- SyncUpRelay/app/seeds/__init__.py
+++ SyncUpRelay/app/seeds/__init__.py
@@ -1,36 +1,43 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
-
 from .servers import seed_servers, undo_servers
+from .channels import seed_channels, undo_channels
+from .messages import seed_messages, undo_messages
+from .reactions import seed_reactions, undo_reactions
 
 from app.models.db import db, environment, SCHEMA
 
 # Creates a seed group to hold our commands
-# So we can type `flask seed --help`
 seed_commands = AppGroup('seed')
 
 
 # Creates the `flask seed all` command
 @seed_commands.command('all')
 def seed():
-    if environment == 'production':
-        # Before seeding in production, you want to run the seed undo
-        # command, which will truncate all tables prefixed with
-        # the schema name (see comment in users.py undo_users function).
-        # Make sure to add all your other model's undo functions below
-        undo_users()
-        # Add the undo_servers function for production
-        undo_servers()
+    # Before seeding in production, you want to run the seed undo
+    # Run the undo commands first to clear the database tables.
+    # The undo functions are called in reverse order of model creation
+    undo_reactions()
+    undo_messages()
+    undo_channels()
+    undo_servers()
+    undo_users()
+
+    # --- Seed the database tables ---
     seed_users()
-    # 2. Call your server seeder function
     seed_servers()
+    seed_channels()
+    seed_messages()
+    seed_reactions()
     # Add other seed functions here
 
 
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
-    undo_users()
-    # 3. Call the undo function
+    # Run undo commands in reverse order of creation
+    undo_reactions()
+    undo_messages()
+    undo_channels()
     undo_servers()
-    # Add other undo functions here
+    undo_users()
