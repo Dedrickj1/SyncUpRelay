d216343305d495c006dcf0f3c813f5f7d698b66e
Dedrickj1 <dedrickjackson11@gmail.com>
2025-08-12 20:20:37 -0500

    websocket update

--- SyncUpRelay/app/__init__.py
+++ SyncUpRelay/app/__init__.py
@@ -4,12 +4,20 @@ from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect, generate_csrf
 from flask_login import LoginManager
+from flask_socketio import SocketIO
 from .models import db, User
 from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
 from .seeds import seed_commands
 from .config import Config
 from .api.server_routes import server_routes
+
+if os.environ.get('FLASK_ENV') == 'production':
+    origins = [
+        # Add your production frontend url here
+    ]
+else:
+    origins = "*"
 
 app = Flask(__name__, static_folder='../react-vite/dist', static_url_path='/')
 
@@ -35,6 +43,12 @@ Migrate(app, db)
 
 # Application Security
 CORS(app)
+
+# Initialize SocketIO with CORS
+socketio = SocketIO(app, cors_allowed_origins=origins)
+
+# Import sockets AFTER initializing socketio
+from .sockets import *
 
 
 # Since we are deploying with Docker and Flask,
--- SyncUpRelay/react-vite/src/router/Layout.jsx
+++ SyncUpRelay/react-vite/src/router/Layout.jsx
@@ -4,13 +4,31 @@ import { useDispatch } from "react-redux";
 import { ModalProvider, Modal } from "../context/Modal";
 import { thunkAuthenticate } from "../redux/session";
 import Navigation from "../components/Navigation/Navigation";
+import { useSocket } from "../context/SocketContext";
 
 export default function Layout() {
   const dispatch = useDispatch();
   const [isLoaded, setIsLoaded] = useState(false);
+  const socket = useSocket(); 
+
   useEffect(() => {
     dispatch(thunkAuthenticate()).then(() => setIsLoaded(true));
   }, [dispatch]);
+
+  // Add a new useEffect to handle the WebSocket connection
+  useEffect(() => {
+    // Make sure the socket is connected before using it
+    if (socket) {
+      // Listen for the "connect" event
+      socket.on('connect', () => {
+        console.log('Successfully connected to WebSocket server with ID:', socket.id);
+      });
+
+      return () => {
+        socket.off('connect');
+      };
+    }
+  }, [socket]); // This effect runs whenever the socket object changes
 
   return (
     <>
